fd922e347e66d104c490d70abff7984e05bf7c9a
Chris-Slade <Chris-Slade@users.noreply.github.com>
2023-08-14 20:22:50 +0000

    Deploying to gh-pages from @ Chris-Slade/dpr-calc@bc94dc557cdd5b46ec9e84947f7668c3bf5a3cbe 🚀

--- _next/static/MQu1C0KyNF9NvbuvR4ued/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/215-9b9a12897c97140b.js","static/chunks/pages/index-82fb7a8272a6c411.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/yAX2jMx4m7NHU0aFsUHUj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/215-9b9a12897c97140b.js","static/chunks/pages/index-476e956bf7a8b88b.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
